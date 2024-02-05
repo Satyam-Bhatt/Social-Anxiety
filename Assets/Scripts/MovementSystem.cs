5eb5ba941e08ae8258cf00a34a9f84cb8ad86742
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-02-05 15:09:50 +0530

    Added Movement

--- Assets/Scripts/MovementSystem.cs
+++ Assets/Scripts/MovementSystem.cs
@@ -6,17 +6,32 @@ using UnityEngine.InputSystem;
 public class MovementSystem : MonoBehaviour
 {
     private PlayerControls playerControls;
+    private Rigidbody2D rb;
+
+    private Vector2 moveInput = Vector2.zero;
+
+    [SerializeField]
+    private float moveSpeed = 10f;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
     }
     private void OnEnable()
     {
         playerControls.Movement.Move.Enable();
-        playerControls.Movement.Move.performed += OnMove;
+
         playerControls.Movement.Interact.Enable();
         playerControls.Movement.Interact.performed += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Movement.Move.Disable();
+
+        playerControls.Movement.Interact.Disable();
+        playerControls.Movement.Interact.performed -= OnJump;
     }
 
     // Start is called before the first frame update
@@ -26,14 +41,10 @@ public class MovementSystem : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
-
-    public void OnMove(InputAction.CallbackContext context)   
-    {
-        Debug.Log(playerControls.Movement.Move.ReadValue<Vector2>());
+        moveInput = playerControls.Movement.Move.ReadValue<Vector2>();
+        rb.velocity = moveInput * moveSpeed;
     }
 
     public void OnJump(InputAction.CallbackContext context)
