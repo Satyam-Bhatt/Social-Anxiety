513a366c20e26b3e47726d86e87c6af1142a476a
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-02-08 11:37:40 +0530

    Random Eye Spawner and Eye Movement Added

---
+++ Assets/EyeMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeMovement : MonoBehaviour
+{
+    [SerializeField] private float down_Speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y_posi = transform.position.y;
+        y_posi -= down_Speed *Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, y_posi, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/EyeSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EyeSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] eye_SpawnPoint;
+    [SerializeField] private GameObject eye;
+    [SerializeField] private float spawnTime = 2f;
+
+    private void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    private int RandomPicker()
+    {
+        return Random.Range(0, eye_SpawnPoint.Length);
+    }
+
+    private IEnumerator Spawner()
+    {
+        while (true)
+        {
+            int spawnPoint_1 = RandomPicker();
+            int spawnPoint_2 = RandomPicker();
+            while(spawnPoint_1 == spawnPoint_2) spawnPoint_2 = RandomPicker();
+            Instantiate(eye, eye_SpawnPoint[spawnPoint_1].position, Quaternion.identity);
+            Instantiate(eye, eye_SpawnPoint[spawnPoint_2].position, Quaternion.identity);
+
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
