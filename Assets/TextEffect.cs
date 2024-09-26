5dc8a53ea9fdf01b736b0f077848110864822957
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-09-26 23:29:35 +0530

    Text hua animate

--- Assets/TextEffect.cs
+++ Assets/TextEffect.cs
@@ -6,11 +6,14 @@ using TMPro;
 public class TextEffect : MonoBehaviour
 {
     private TMP_Text text;
+    private Vector3[] verts;
 
     private void Awake()
     {
         text = GetComponent<TMP_Text>();
     }
+
+
 
     private void Update()
     {
@@ -18,16 +21,72 @@ public class TextEffect : MonoBehaviour
 
         TMP_TextInfo textInfo = text.textInfo;
 
-        Vector3[] verts = text.mesh.vertices;
+        verts = text.mesh.vertices;
 
         int vertCount = 0;
-        for (int i = 0; i < textInfo.characterCount; i++)
+        for (int i = 0; i < verts.Length; i++)
         {
-            verts[0 + i] = textInfo.characterInfo[i].bottomLeft;
-            verts[1 + i] = textInfo.characterInfo[i].topLeft;
-            verts[2 + i] = textInfo.characterInfo[i].topRight;
-            verts[3 + i] = textInfo.characterInfo[i].bottomRight;
+            Vector3 offset = Vector3.zero;
+            offset.y = Mathf.Sin((Time.time * 10 + verts[i].x * 2) / 10f) * 5;
+
+            verts[i] += offset;
+
+/*            verts[vertCount + 0] += new Vector3(0, Mathf.Sin(Time.time * 5f) * 13f + 13f, 0);
+            verts[vertCount + 1] += new Vector3(0, Mathf.Sin(Time.time * 5f) * 13f + 13f, 0);
+            verts[vertCount + 2] += new Vector3(0, Mathf.Sin(Time.time * 5f) * 13f + 13f, 0);
+            verts[vertCount + 3] += new Vector3(0, Mathf.Sin(Time.time * 5f) * 13f + 13f, 0);
+
+            vertCount += 4;*/
         }
-        
+
+        text.mesh.vertices = verts;
+        text.canvasRenderer.SetMesh(text.mesh);
+
+    }
+
+/*    TMP_Text textMesh;
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.ForceMeshUpdate();
+        mesh = textMesh.mesh;
+        vertices = mesh.vertices;
+
+        foreach (Vector3 vertex in vertices)
+        {
+            Debug.Log("1: " + vertex);
+        }
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 offset = Wobble(Time.time + i);
+
+            vertices[i] = vertices[i] + offset;
+        }
+
+        mesh.vertices = vertices;
+
+        foreach (Vector3 vertex in mesh.vertices)
+        {
+            Debug.Log("2: " + vertex);
+        }
+
+        textMesh.canvasRenderer.SetMesh(mesh);
+    }*/
+
+    Vector2 Wobble(float time)
+    {
+        return new Vector2(Mathf.Sin(time * 3.3f), Mathf.Cos(time * 2.5f));
     }
 }
